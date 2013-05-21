d10b6fb829154359e688731ce0c1126bd2f1164c
Jacob Greenleaf <greenleaf.jacob@gmail.com>
2013-05-21 16:10:59 -0500

    Only enumerate over our own properties (Asafh). Use innerHTML rather than textContent because it has better browser support.

--- ThinDOM.js
+++ ThinDOM.js
@@ -3,11 +3,15 @@
  */
 function ThinDOM(tag, attributes) {
     this.el = document.createElement(tag);
-	if(typeof attributes != 'undefined') {
-		this.attr(attributes);
-	}
+    if(typeof attributes != 'undefined') {
+        this.attr(attributes);
+    }
 }
 
+/**
+ * Iterate over all of the element's attributes. 
+ * @param cb(key, value)
+ */
 ThinDOM.prototype.append = (function(other) {
     if(other instanceof ThinDOM) {
         this.el.appendChild(other.get());
@@ -27,26 +31,44 @@ ThinDOM.prototype.append = (function(other) {
     
     return this;
 });
+
+/**
+ * Set the element's style attributes
+ */
 ThinDOM.prototype.css = (function(properties, value) {
     if(properties.constructor === String) {
         this.el.style.properties = value;
     } else if (properties instanceof Object) {
         for(var key in properties) {
-            this.el.style.key = properties[key];
+            if(properties.hasOwnProperty(key)) {
+                this.el.style.key = properties[key];
+            }
         }
     }
     return this;
 });
-ThinDOM.prototype.text = (function(txt) {
-    this.el.textContent = txt;
-    return this;
+
+/**
+ * Set the inner HTML of the element.
+ */
+ThinDOM.prototype.html = (function(html) {
+    if(typeof html == 'undefined') {
+        return this.el.innerHTML;
+    }
+    else {
+        this.el.innerHTML = html;
+        return this;
+    }
 });
+
 ThinDOM.prototype.attr = (function(properties, value) {
     if(properties.constructor === String) {
         this.el.setAttribute(properties, value);
     } else if (properties instanceof Object) {
         for(var key in properties) {
-            this.el.setAttribute(key, properties[key]);
+            if(properties.hasOwnProperty(key)) {
+                this.el.setAttribute(key, properties[key]);
+            }
         }
     }
     return this;
