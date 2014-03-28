6b0202436d916576fd8c11774d29ab528edd536f
Christopher Froehlich <crf@luddites.me>
2014-03-28 13:31:33 -0400

    Refactored to CoffeeScript.

---
+++ dist/thinDOM.js
@@ -0,0 +1,136 @@
+/**
+ * ThinDOM - Inspired by jQuery, this simple library lets you create DOM elements really fast, with significantly more expressiveness than native DOM methods.
+ * @version v0.0.1
+ * @link https://github.com/somecallmechief/ThinDOM
+ * @license 
+ */
+
+/*
+A little thin DOM wrapper with chaining
+ */
+
+(function() {
+  var ThinDOM;
+
+  ThinDOM = function(tag, attributes) {
+    this.el = document.createElement(tag);
+    if (attributes) {
+      this.attr(attributes);
+    }
+  };
+
+
+  /*
+  Iterate over all of the element's attributes.
+  @param cb(key, value)
+   */
+
+  ThinDOM.prototype.append = (function(other) {
+    var self;
+    self = this;
+    if (other instanceof ThinDOM) {
+      self.el.appendChild(other.get());
+    } else if (other instanceof jQuery) {
+      if (other.length > 1) {
+        self = this;
+        _.forEach(other, function(i, otherEl) {
+          self.el.appendChild(otherEl);
+        });
+      } else {
+        self.el.appendChild(other[0]);
+      }
+    } else {
+      if (_.isElement(other)) {
+        self.el.appendChild(other);
+      }
+    }
+    return self;
+  });
+
+
+  /*
+  Set the element's style attributes
+   */
+
+  ThinDOM.prototype.css = (function(properties, value) {
+    var self;
+    self = this;
+    if (properties.constructor === String) {
+      self.el.style.properties = value;
+    } else if (properties instanceof Object) {
+      _.forOwn(properties, function(val, key) {
+        if (!_.isEmpty(val)) {
+          self.el.style.key = val;
+        }
+      });
+    }
+    return self;
+  });
+
+
+  /*
+  Set the inner HTML of the element.
+   */
+
+  ThinDOM.prototype.html = (function(html) {
+    var ret, self;
+    ret = void 0;
+    self = this;
+    if (!html) {
+      ret = self.el.innerHTML;
+    } else {
+      self.el.innerHTML = html;
+      ret = self;
+    }
+    return ret;
+  });
+
+
+  /*
+  Set the inner text of the element as a Text Node
+   */
+
+  ThinDOM.prototype.text = (function(text) {
+    var ret, self, t;
+    ret = void 0;
+    self = this;
+    if (!text) {
+      ret = self.el.innerHTML;
+    } else {
+      t = document.createTextNode(text);
+      self.el.appendChild(t);
+      ret = self;
+    }
+    return ret;
+  });
+
+
+  /*
+  Set attributes on the element
+   */
+
+  ThinDOM.prototype.attr = (function(properties, value) {
+    var self;
+    self = this;
+    if (_.isString(properties)) {
+      self.el.setAttribute(properties, value);
+    } else if (_.isObject(properties)) {
+      _.forOwn(properties, function(val, key) {
+        if (!_.isEmpty(val)) {
+          self.el.setAttribute(key, val);
+        }
+      });
+    }
+    return self;
+  });
+
+
+  /*
+  Get the HTML Element
+   */
+
+  ThinDOM.prototype.get = (function() {
+    return this.el;
+  });
+
+}).call(this);
---
+++ src/ThinDOM.coffee
@@ -0,0 +1,94 @@
+###
+A little thin DOM wrapper with chaining
+###
+ThinDOM = (tag, attributes) ->
+  @el = document.createElement(tag)
+  @attr attributes  if attributes
+  return
+
+###
+Iterate over all of the element's attributes.
+@param cb(key, value)
+###
+ThinDOM::append = ((other) ->
+  self = this
+  if other instanceof ThinDOM
+    self.el.appendChild other.get()
+  else if other instanceof jQuery
+    if other.length > 1
+      self = this
+      _.forEach other, (i, otherEl) ->
+        self.el.appendChild otherEl
+        return
+
+    else
+      self.el.appendChild other[0]
+  else self.el.appendChild other  if _.isElement(other)
+  self
+)
+
+###
+Set the element's style attributes
+###
+ThinDOM::css = ((properties, value) ->
+  self = this
+  if properties.constructor is String
+    self.el.style.properties = value
+  else if properties instanceof Object
+    _.forOwn properties, (val, key) ->
+      self.el.style.key = val  unless _.isEmpty(val)
+      return
+
+  self
+)
+
+###
+Set the inner HTML of the element.
+###
+ThinDOM::html = ((html) ->
+  ret = undefined
+  self = this
+  unless html
+    ret = self.el.innerHTML
+  else
+    self.el.innerHTML = html
+    ret = self
+  ret
+)
+
+###
+Set the inner text of the element as a Text Node
+###
+ThinDOM::text = ((text) ->
+  ret = undefined
+  self = this
+  unless text
+    ret = self.el.innerHTML
+  else
+    t = document.createTextNode(text)
+    self.el.appendChild t
+    ret = self
+  ret
+)
+
+###
+Set attributes on the element
+###
+ThinDOM::attr = ((properties, value) ->
+  self = this
+  if _.isString(properties)
+    self.el.setAttribute properties, value
+  else if _.isObject(properties)
+    _.forOwn properties, (val, key) ->
+      self.el.setAttribute key, val  unless _.isEmpty(val)
+      return
+
+  self
+)
+
+###
+Get the HTML Element
+###
+ThinDOM::get = (->
+  @el
+)
