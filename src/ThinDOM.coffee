0d254a6ca7ea7f9c553ddf97557f8befd4d217b0
Christopher Froehlich <crf@luddites.me>
2014-03-29 21:25:21 -0400

    Refactored to remove reliance on prototype. Volatile this eliminated. Instance is always self-aware.

--- src/ThinDOM.coffee
+++ src/ThinDOM.coffee
@@ -11,108 +11,119 @@ removeMethod = (->
       'valueOf'
 )()
 
+prop = (obj, name, value, writable = false, configurable = false, enumerable = false) ->
+  throw new Error("Cannot define a property without an Object.")  unless obj
+  throw new Error("Cannot create a property without a valid property name.")  unless typeof name is "string"
+  
+  Object.defineProperty obj, name,
+    value: value
+    writable: writable
+    configurable: configurable
+    enumerable: enumerable
+  obj
+
+obj = (properties = {}, inheritsFromPrototype = null) ->
+  obj = Object.create(inheritsFromPrototype, properties)
+  
+  prop obj, 'add', ((name, val, writable, configurable, enumerable) ->
+    prop obj, name, val, writable, configurable, enumerable
+  ), false, false, false
+  obj  
+  
 ###
 A little thin DOM wrapper with chaining
 ###
 ThinDOM = (tag, attributes, el = null) ->
-  @el = el or document.createElement(tag)
-  @attr attributes if attributes
-  return
+  ret = obj()
+  ret.add 'el', el or document.createElement(tag)
 
-###
-Append one element to another
-###
-ThinDOM::append = ((other) ->
-  self = this
-  if other instanceof ThinDOM
-    self.el.appendChild other.get()
-  else if other instanceof jQuery
-    if other.length > 1
-      self = this
-      _.forEach other, (i, otherEl) ->
-        self.el.appendChild otherEl
+	###
+	Append one element to another
+	###
+	ret.add 'append', (other) ->
+	  self = ret
+	  if other instanceof ThinDOM
+		  self.el.appendChild other.get()
+	  else if other instanceof jQuery
+		  if other.length > 1
+		    _.forEach other, (i, otherEl) ->
+			    self.el.appendChild otherEl
+			    return
+		  else
+		    self.el.appendChild other[0]
+	  else self.el.appendChild other  if _.isElement(other)
+	  self
+  
+  ###
+  Remove the element
+  ###
+  ret.add 'remove', ->
+    self = ret
+    self.el[removeMethod]()
+    return
+  
+  ###
+  Set the element's style attributes
+  ###
+  ret.add 'css', (properties, value) ->
+    self = ret
+    if properties.constructor is String
+      self.el.style.properties = value
+    else if properties instanceof Object
+      _.forOwn properties, (val, key) ->
+        self.el.style.key = val  unless _.isEmpty(val)
         return
 
+    self
+
+  ###
+  Set the inner HTML of the element.
+  ###
+  ret.add 'html', (html) ->
+    val = undefined
+    self = ret
+    unless html
+      val = self.el.innerHTML
     else
-      self.el.appendChild other[0]
-  else self.el.appendChild other  if _.isElement(other)
-  self
-)
+      self.el.innerHTML = html
+      val = self
+    val
+  
+  ###
+  Set the inner text of the element as a Text Node
+  ###
+  ret.add 'text', (text) ->
+    val = undefined
+    self = ret
+    unless text
+      val = self.el.innerHTML
+    else
+      t = document.createTextNode(text)
+      self.el.appendChild t
+      val = self
+    val
+    
+  ###
+  Set attributes on the element
+  ###
+  ret.add 'attr', (properties, value) ->
+    self = ret
+    if _.isString(properties)
+      self.el.setAttribute properties, value
+    else if _.isObject(properties)
+      _.forOwn properties, (val, key) ->
+        self.el.setAttribute key, val  unless _.isEmpty(val)
+        return
 
-###
-Remove the element
-###
-ThinDOM::remove = (() ->
-  self = this
-  self.el[removeMethod]()
-  return
-)
-
-###
-Set the element's style attributes
-###
-ThinDOM::css = ((properties, value) ->
-  self = this
-  if properties.constructor is String
-    self.el.style.properties = value
-  else if properties instanceof Object
-    _.forOwn properties, (val, key) ->
-      self.el.style.key = val  unless _.isEmpty(val)
-      return
-
-  self
-)
-
-###
-Set the inner HTML of the element.
-###
-ThinDOM::html = ((html) ->
-  ret = undefined
-  self = this
-  unless html
-    ret = self.el.innerHTML
-  else
-    self.el.innerHTML = html
-    ret = self
+    self
+  
+  ###
+  Get the HTML Element
+  ###
+  ret.add 'get', ->
+    ret.el
+  
+  if attributes then ret.attr attributes
   ret
-)
-
-###
-Set the inner text of the element as a Text Node
-###
-ThinDOM::text = ((text) ->
-  ret = undefined
-  self = this
-  unless text
-    ret = self.el.innerHTML
-  else
-    t = document.createTextNode(text)
-    self.el.appendChild t
-    ret = self
-  ret
-)
-
-###
-Set attributes on the element
-###
-ThinDOM::attr = ((properties, value) ->
-  self = this
-  if _.isString(properties)
-    self.el.setAttribute properties, value
-  else if _.isObject(properties)
-    _.forOwn properties, (val, key) ->
-      self.el.setAttribute key, val  unless _.isEmpty(val)
-      return
-
-  self
-)
-
-###
-Get the HTML Element
-###
-ThinDOM::get = (->
-  @el
-)
-
 
 thisGlobal.ThinDOM = ThinDOM
--- src/thinDOM.js
+++ src/thinDOM.js
@@ -1,11 +1,11 @@
 /**
  * thindom - Inspired by jQuery, this simple library lets you create DOM elements really fast, with significantly more expressiveness than native DOM methods.
- * @version v0.0.5
+ * @version v0.0.6
  * @link https://github.com/somecallmechief/ThinDOM
  * @license 
  */
 (function() {
-  var ThinDOM, removeMethod, thisGlobal;
+  var ThinDOM, obj, prop, removeMethod, thisGlobal;
 
   thisGlobal = (typeof global !== 'undefined' && global ? global : (typeof window !== 'undefined' ? window : this));
 
@@ -23,34 +23,71 @@
     }
   })();
 
+  prop = function(obj, name, value, writable, configurable, enumerable) {
+    if (writable == null) {
+      writable = false;
+    }
+    if (configurable == null) {
+      configurable = false;
+    }
+    if (enumerable == null) {
+      enumerable = false;
+    }
+    if (!obj) {
+      throw new Error("Cannot define a property without an Object.");
+    }
+    if (typeof name !== "string") {
+      throw new Error("Cannot create a property without a valid property name.");
+    }
+    Object.defineProperty(obj, name, {
+      value: value,
+      writable: writable,
+      configurable: configurable,
+      enumerable: enumerable
+    });
+    return obj;
+  };
+
+  obj = function(properties, inheritsFromPrototype) {
+    if (properties == null) {
+      properties = {};
+    }
+    if (inheritsFromPrototype == null) {
+      inheritsFromPrototype = null;
+    }
+    obj = Object.create(inheritsFromPrototype, properties);
+    prop(obj, 'add', (function(name, val, writable, configurable, enumerable) {
+      return prop(obj, name, val, writable, configurable, enumerable);
+    }), false, false, false);
+    return obj;
+  };
+
 
   /*
   A little thin DOM wrapper with chaining
    */
 
   ThinDOM = function(tag, attributes, el) {
+    var ret;
     if (el == null) {
       el = null;
     }
-    this.el = el || document.createElement(tag);
-    if (attributes) {
-      this.attr(attributes);
-    }
+    ret = obj();
+    return ret.add('el', el || document.createElement(tag));
   };
 
 
   /*
-  Append one element to another
+  	Append one element to another
    */
 
-  ThinDOM.prototype.append = (function(other) {
+  ret.add('append', function(other) {
     var self;
-    self = this;
+    self = ret;
     if (other instanceof ThinDOM) {
       self.el.appendChild(other.get());
     } else if (other instanceof jQuery) {
       if (other.length > 1) {
-        self = this;
         _.forEach(other, function(i, otherEl) {
           self.el.appendChild(otherEl);
         });
@@ -70,9 +107,9 @@
   Remove the element
    */
 
-  ThinDOM.prototype.remove = (function() {
+  ret.add('remove', function() {
     var self;
-    self = this;
+    self = ret;
     self.el[removeMethod]();
   });
 
@@ -81,9 +118,9 @@
   Set the element's style attributes
    */
 
-  ThinDOM.prototype.css = (function(properties, value) {
+  ret.add('css', function(properties, value) {
     var self;
-    self = this;
+    self = ret;
     if (properties.constructor === String) {
       self.el.style.properties = value;
     } else if (properties instanceof Object) {
@@ -101,17 +138,17 @@
   Set the inner HTML of the element.
    */
 
-  ThinDOM.prototype.html = (function(html) {
-    var ret, self;
-    ret = void 0;
-    self = this;
+  ret.add('html', function(html) {
+    var self, val;
+    val = void 0;
+    self = ret;
     if (!html) {
-      ret = self.el.innerHTML;
+      val = self.el.innerHTML;
     } else {
       self.el.innerHTML = html;
-      ret = self;
+      val = self;
     }
-    return ret;
+    return val;
   });
 
 
@@ -119,18 +156,18 @@
   Set the inner text of the element as a Text Node
    */
 
-  ThinDOM.prototype.text = (function(text) {
-    var ret, self, t;
-    ret = void 0;
-    self = this;
+  ret.add('text', function(text) {
+    var self, t, val;
+    val = void 0;
+    self = ret;
     if (!text) {
-      ret = self.el.innerHTML;
+      val = self.el.innerHTML;
     } else {
       t = document.createTextNode(text);
       self.el.appendChild(t);
-      ret = self;
+      val = self;
     }
-    return ret;
+    return val;
   });
 
 
@@ -138,9 +175,9 @@
   Set attributes on the element
    */
 
-  ThinDOM.prototype.attr = (function(properties, value) {
+  ret.add('attr', function(properties, value) {
     var self;
-    self = this;
+    self = ret;
     if (_.isString(properties)) {
       self.el.setAttribute(properties, value);
     } else if (_.isObject(properties)) {
@@ -158,9 +195,15 @@
   Get the HTML Element
    */
 
-  ThinDOM.prototype.get = (function() {
-    return this.el;
+  ret.add('get', function() {
+    return ret.el;
   });
+
+  if (attributes) {
+    ret.attr(attributes);
+  }
+
+  ret;
 
   thisGlobal.ThinDOM = ThinDOM;
 
