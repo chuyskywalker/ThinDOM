1877158b5b9b1241c52c28432bda2bbb959faf5a
Aaron Hamid <aaron.hamid@gmail.com>
2014-06-15 01:04:19 -0400

    add test demonstrating bug in #css

--- test/tests.js
+++ test/tests.js
@@ -7,3 +7,13 @@ QUnit.test("#html", function(assert) {
   assert.ok(td_el.html() == "", "Successfully cleared innert html");
   assert.ok(td_el.el.innerHTML == "", "Successfully cleared innert html");
 });
+
+QUnit.test('#css', function(assert) {
+  var td_el = new ThinDOM('span');
+  td_el.css('display', 'none');
+  assert.ok(td_el.el.style['display'] == 'none', 'Successfully set individual style');
+  td_el.css({ display: '', color: 'red', width: 'auto' });
+  assert.ok(td_el.el.style['display'] == 'none' && // does not unset empty values
+            td_el.el.style['color'] == 'red' &&
+            td_el.el.style['width'] == 'auto', 'Successfully set bulk styles');
+});
